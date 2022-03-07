4a36d09e5fd764cae366313a66f9bd72185cedeb
prajavalpatki <prajavalpatki.com>
2022-03-08 02:02:52 +0530

    Total working hours or days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,19 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int RATE_PER_HRS = 20;
-    public static final int NUM_OF_DAYS = 20;
+    public static final int MAX_NUM_OF_DAYS = 20;
+    public static final int MAX_NUM_OF_HRS = 100;
     public static void main(String[] args) {
         int empHrs = 0;
-        int empWage = 0;
         int totalEmpWage = 0;
-        for(int day = 1; day <= NUM_OF_DAYS; day++) {
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+        while( totalEmpHrs < MAX_NUM_OF_HRS && totalWorkingDays < MAX_NUM_OF_DAYS ) {
+            totalWorkingDays++;
             double empCheck = Math.floor(Math.random()* 10) % 3;
             int x = (int) empCheck;
             switch (x) {
@@ -22,10 +26,10 @@ public class EmployeeWage {
                 default :
                     empHrs = 0;
             }
-            empWage = empHrs * RATE_PER_HRS;
-            totalEmpWage += empWage;
-            System.out.println("Emp Daily Wage = " +empWage);
+            totalEmpHrs += empHrs;
+            System.out.println(" Day - " +totalWorkingDays+ " Emp Hrs - " +empHrs);
         }
-        System.out.println(" so Employee 20 days Wage = "+totalEmpWage);
+        totalEmpWage = totalEmpHrs * RATE_PER_HRS;
+        System.out.println("Employee Wage = "+totalEmpWage);
     }
 }
