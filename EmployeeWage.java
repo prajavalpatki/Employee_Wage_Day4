4864e6c72774d07fc24d512a76281ed6b0ecbe25
prajavalpatki <prajavalpatki.com>
2022-03-08 01:59:46 +0530

    Calculate wages for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,22 +4,28 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int RATE_PER_HRS = 20;
+    public static final int NUM_OF_DAYS = 20;
     public static void main(String[] args) {
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random()* 10) % 3;
-        int x = (int) empCheck;
-        switch (x) {
-            case IS_PART_TIME :
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME :
-                empHrs = 8;
-                break;
-            default :
-                empHrs = 0;
+        int totalEmpWage = 0;
+        for(int day = 1; day <= NUM_OF_DAYS; day++) {
+            double empCheck = Math.floor(Math.random()* 10) % 3;
+            int x = (int) empCheck;
+            switch (x) {
+                case IS_PART_TIME :
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME :
+                    empHrs = 8;
+                    break;
+                default :
+                    empHrs = 0;
+            }
+            empWage = empHrs * RATE_PER_HRS;
+            totalEmpWage += empWage;
+            System.out.println("Emp Daily Wage = " +empWage);
         }
-        empWage = empHrs * RATE_PER_HRS;
-        System.out.println("Employe Hours is " +empHrs+ " so Employee Wage = "+empWage);
+        System.out.println(" so Employee 20 days Wage = "+totalEmpWage);
     }
 }
