f2b72dc0ea37aaa72fb2a37a8421c2b1e47c2605
prajavalpatki <prajavalpatki.com>
2022-03-08 01:55:49 +0530

    Switch case statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,25 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int RATE_PER_HRS = 20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
-
-        int isPartTime = 1;
-        int isFullTime = 2;
-        int ratePerHrs = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck= Math.floor(Math.random()* 10) % 3;
-        if (empCheck == isPartTime)
-            empHrs = 4;
-        else if (empCheck == isFullTime)
-            empHrs = 8;
-        else
-            empHrs = 0;
-        empWage = empHrs * ratePerHrs;
-        System.out.println("Employee Wage = "+empWage);
+        double empCheck = Math.floor(Math.random()* 10) % 3;
+        int x = (int) empCheck;
+        switch (x) {
+            case IS_PART_TIME :
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME :
+                empHrs = 8;
+                break;
+            default :
+                empHrs = 0;
+        }
+        empWage = empHrs * RATE_PER_HRS;
+        System.out.println("Employe Hours is " +empHrs+ " so Employee Wage = "+empWage);
     }
 }
